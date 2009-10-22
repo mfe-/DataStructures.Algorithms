09a0565f99804a3a3baf780b200a4d46584f9024
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2009-10-22 12:43:41 +0000

    Get.UI.Controls
    git-svn-id: http://10.51.51.252/svn/Get@8 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

---
+++ GTS/UI/Get.UI.Controls/ObjectControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Markup;
+using Get.Model.Core;
+using System.ComponentModel;
+
+[assembly: XmlnsDefinition("http://schemas.get.com/winfx/2009/xaml/Controls", "Get.Controls")]
+namespace Get.Controls
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// http://msdn.microsoft.com/de-de/library/system.windows.markup.contentpropertyattribute.aspx
+    /// </summary>
+    [ContentProperty("ItemsSource")]
+    [LocalizabilityAttribute(LocalizationCategory.None, Readability = Readability.Unreadable)]
+    //[StyleTypedPropertyAttribute(Property = "ItemContainerStyle", StyleTargetType = typeof(FrameworkElement))]
+    public partial class ObjectControl : UserControl
+    {
+        public ObjectControl()
+        {
+            InitializeComponent();
+        }
+
+        [BindableAttribute(true)]
+        public ObjectCore ItemsSource
+        {
+            get { return (ObjectCore)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ItemsSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(ObjectCore), typeof(ObjectControl), new UIPropertyMetadata(null));
+
+
+    }
+}
