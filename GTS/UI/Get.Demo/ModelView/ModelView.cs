c3bc1248222727ac6958f0de25b44b61939af5a9
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2009-10-23 16:36:56 +0000

    Auskommentierung Rückgänig gemacht
    git-svn-id: http://10.51.51.252/svn/Get@15 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

--- GTS/UI/Get.Demo/ModelView/ModelView.cs
+++ GTS/UI/Get.Demo/ModelView/ModelView.cs
@@ -4,60 +4,61 @@ using System.Linq;
 using System.Text;
 using System.ComponentModel;
 using Get.Common;
+using Get.Model.Core;
 
 namespace Get.Demo
 {
     public class ModelView : INotifyPropertyChanged
     {
-        //public ModelView()
-        //{
-        //    ObjectCoreList = new ObjectCoreList();
-        //    ObjectCoreList.Add(new ObjectCore() { ObjectName = "Kontakt" });
-        //    ObjectCore = new ObjectCore() { ObjectName = "Kontakt" };
-        //    ObjectCore objectCore = new ObjectCore() { ObjectName = "Base" };
-        //    ObjectCore.DerivedObject = objectCore;
-        //    ObjectCore.ObjectPropertieList.Add(new ObjectPropertie() { PropertieName = "Vorname" });
-        //    ObjectCore.ObjectPropertieList.Add(new ObjectPropertie() { PropertieName = "Nachname" });
-        //}
-        //public ObjectCoreList ObjectCoreList { get; set; }
+        public ModelView()
+        {
+            ObjectCoreList = new ObjectCoreList();
+            ObjectCoreList.Add(new ObjectCore() { ObjectName = "Kontakt" });
+            ObjectCore = new ObjectCore() { ObjectName = "Kontakt" };
+            ObjectCore objectCore = new ObjectCore() { ObjectName = "Base" };
+            ObjectCore.DerivedObject = objectCore;
+            ObjectCore.ObjectPropertieList.Add(new ObjectPropertie() { PropertieName = "Vorname" });
+            ObjectCore.ObjectPropertieList.Add(new ObjectPropertie() { PropertieName = "Nachname" });
+        }
+        public ObjectCoreList ObjectCoreList { get; set; }
 
-        //private string _PropertieModifiers;
-        //public string PropertieModifiers
-        //{
-        //    get
-        //    {
-        //        return _PropertieModifiers;
-        //    }
-        //    set
-        //    {
-        //        _PropertieModifiers = value;
-        //        System.Diagnostics.Stopwatch s = new System.Diagnostics.Stopwatch();
-        //        s.Start();
-        //        NotifyPropertyChanged(this.GetMemberName(x=>x.PropertieModifiers));
-        //        s.Stop();
+        private string _PropertieModifiers;
+        public string PropertieModifiers
+        {
+            get
+            {
+                return _PropertieModifiers;
+            }
+            set
+            {
+                _PropertieModifiers = value;
+                System.Diagnostics.Stopwatch s = new System.Diagnostics.Stopwatch();
+                s.Start();
+                NotifyPropertyChanged(this.GetMemberName(x => x.PropertieModifiers));
+                s.Stop();
 
-        //    }
-        //}
+            }
+        }
 
-        //private string _text;
-        //public string text
-        //{
-        //    get
-        //    {
-        //        return _text;
-        //    }
-        //    set
-        //    {
-        //        _text = value;
-        //        System.Diagnostics.Stopwatch s = new System.Diagnostics.Stopwatch();
-        //        s.Start();
-        //        NotifyPropertyChanged("text");
-        //        s.Stop();
+        private string _text;
+        public string text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+                System.Diagnostics.Stopwatch s = new System.Diagnostics.Stopwatch();
+                s.Start();
+                NotifyPropertyChanged("text");
+                s.Stop();
 
-        //    }
-        //}
+            }
+        }
 
-        //public ObjectCore ObjectCore { get; set; }
+        public ObjectCore ObjectCore { get; set; }
 
         #region INotifyPropertyChanged Member
 
