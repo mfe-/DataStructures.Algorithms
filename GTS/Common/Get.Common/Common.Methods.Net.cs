855a0c7be10a4a9fffb7224e0a38f560b085400d
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2009-11-11 12:58:16 +0000

    Klasse hinzugefügt in der später alle relevanten Net Funktion liegen
    git-svn-id: http://10.51.51.252/svn/Get@28 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

---
+++ GTS/Common/Get.Common/Common.Methods.Net.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Get.Common
+{
+    public static class Net
+    {
+        public static int GetFreePort()
+        {
+            return 1;
+        }
+    }
+}
