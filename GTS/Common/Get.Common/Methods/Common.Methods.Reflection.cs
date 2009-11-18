53bdce1b7bc0955d3616243d137f224393d486e0
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2009-11-18 11:59:54 +0000

    PropertySetterHelper und TextBlock Style für Connection Animation hinzugefügt
    git-svn-id: http://10.51.51.252/svn/Get@40 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

---
+++ GTS/Common/Get.Common/Methods/Common.Methods.Reflection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using System.Windows.Markup;
+
+
+namespace Get.Common
+{
+    public static class Reflection
+    {
+        public class PropertySetterHelper
+        {
+            public PropertySetterHelper(object pInstance)
+            {
+                if (pInstance == null)
+                    throw new ArgumentException("Cant be null pInstance");
+
+                _Instance = pInstance;
+            }
+
+            public void SetProperty(string pPropertyName, object PropertyValue)
+            {
+                Type type = _Instance.GetType();
+
+                PropertyInfo propertyInfo = type.GetProperties().ToList().Where(a => a.Name.Equals(pPropertyName)).First();
+                if (propertyInfo == null) return;
+                propertyInfo.SetValue(_Instance, PropertyValue, null);
+            }
+
+            private readonly object _Instance = null;
+
+        }
+
+    }
+}
