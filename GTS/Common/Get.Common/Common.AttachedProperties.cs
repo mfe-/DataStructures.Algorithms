e9d0803c3dda86055435df2158ef40fba8c926de
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2010-02-14 13:12:55 +0000

    ListBoxExtender verschoben
    git-svn-id: http://10.51.51.252/svn/Get@60 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

--- GTS/Common/Get.Common/Common.AttachedProperties.cs
+++ GTS/Common/Get.Common/Common.AttachedProperties.cs
@@ -48,100 +48,98 @@ namespace Get.Common
             e.Handled = true;
         }
 
+
+    }
+    /// <summary>  
+    /// This class contains a few useful extenders for the ListBox  
+    /// ListBox – Automatically scroll CurrentItem into View
+    /// http://michlg.wordpress.com/2010/01/16/listbox-automatically-scroll-currentitem-into-view/
+    /// </summary>  
+    public class ListBoxExtenders : DependencyObject
+    {
+
+        #region Properties
+
+        public static readonly DependencyProperty AutoScrollToCurrentItemProperty = DependencyProperty.RegisterAttached("AutoScrollToCurrentItem", typeof(bool), typeof(ListBoxExtenders), new UIPropertyMetadata(default(bool), OnAutoScrollToCurrentItemChanged));
+
         /// <summary>  
-        /// This class contains a few useful extenders for the ListBox  
-        /// ListBox – Automatically scroll CurrentItem into View
-        /// http://michlg.wordpress.com/2010/01/16/listbox-automatically-scroll-currentitem-into-view/
+        /// Returns the value of the AutoScrollToCurrentItemProperty  
         /// </summary>  
-        public class ListBoxExtenders : DependencyObject
+        /// <param name="obj">The dependency-object whichs value should be returned</param>  
+        /// <returns>The value of the given property</returns>  
+        public static bool GetAutoScrollToCurrentItem(DependencyObject obj)
         {
 
-            #region Properties
+            return (bool)obj.GetValue(AutoScrollToCurrentItemProperty);
 
-            public static readonly DependencyProperty AutoScrollToCurrentItemProperty = DependencyProperty.RegisterAttached("AutoScrollToCurrentItem", typeof(bool), typeof(ListBoxExtenders), new UIPropertyMetadata(default(bool), OnAutoScrollToCurrentItemChanged));
+        }
+        /// <summary>  
+        /// Sets the value of the AutoScrollToCurrentItemProperty  
+        /// </summary>  
+        /// <param name="obj">The dependency-object whichs value should be set</param>  
+        /// <param name="value">The value which should be assigned to the AutoScrollToCurrentItemProperty</param>  
+        public static void SetAutoScrollToCurrentItem(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoScrollToCurrentItemProperty, value);
+        }
 
-            /// <summary>  
-            /// Returns the value of the AutoScrollToCurrentItemProperty  
-            /// </summary>  
-            /// <param name="obj">The dependency-object whichs value should be returned</param>  
-            /// <returns>The value of the given property</returns>  
-            public static bool GetAutoScrollToCurrentItem(DependencyObject obj)
+        #endregion
+
+        #region Events
+
+        /// <summary>  
+        /// This method will be called when the AutoScrollToCurrentItem  
+        /// property was changed  
+        /// </summary>  
+        /// <param name="s">The sender (the ListBox)</param>  
+        /// <param name="e">Some additional information</param>  
+        public static void OnAutoScrollToCurrentItemChanged(DependencyObject s, DependencyPropertyChangedEventArgs e)
+        {
+
+            var listBox = s as ListBox;
+
+            if (listBox != null)
             {
 
-                return (bool)obj.GetValue(AutoScrollToCurrentItemProperty);
+                var listBoxItems = listBox.Items;
 
-            }
-            /// <summary>  
-            /// Sets the value of the AutoScrollToCurrentItemProperty  
-            /// </summary>  
-            /// <param name="obj">The dependency-object whichs value should be set</param>  
-            /// <param name="value">The value which should be assigned to the AutoScrollToCurrentItemProperty</param>  
-            public static void SetAutoScrollToCurrentItem(DependencyObject obj, bool value)
-            {
-                obj.SetValue(AutoScrollToCurrentItemProperty, value);
-            }
-
-            #endregion
-
-            #region Events
-
-            /// <summary>  
-            /// This method will be called when the AutoScrollToCurrentItem  
-            /// property was changed  
-            /// </summary>  
-            /// <param name="s">The sender (the ListBox)</param>  
-            /// <param name="e">Some additional information</param>  
-            public static void OnAutoScrollToCurrentItemChanged(DependencyObject s, DependencyPropertyChangedEventArgs e)
-            {
-
-                var listBox = s as ListBox;
-
-                if (listBox != null)
+                if (listBoxItems != null)
                 {
 
-                    var listBoxItems = listBox.Items;
+                    var newValue = (bool)e.NewValue;
 
-                    if (listBoxItems != null)
-                    {
+                    var autoScrollToCurrentItemWorker = new EventHandler((s1, e2) => OnAutoScrollToCurrentItem(listBox, listBox.Items.CurrentPosition));
 
-                        var newValue = (bool)e.NewValue;
+                    if (newValue)
 
-                        var autoScrollToCurrentItemWorker = new EventHandler((s1, e2) => OnAutoScrollToCurrentItem(listBox, listBox.Items.CurrentPosition));
+                        listBoxItems.CurrentChanged += autoScrollToCurrentItemWorker;
 
-                        if (newValue)
+                    else
 
-                            listBoxItems.CurrentChanged += autoScrollToCurrentItemWorker;
-
-                        else
-
-                            listBoxItems.CurrentChanged -= autoScrollToCurrentItemWorker;
-
-                    }
+                        listBoxItems.CurrentChanged -= autoScrollToCurrentItemWorker;
 
                 }
 
             }
 
-            /// <summary>  
-            /// This method will be called when the ListBox should  
-            /// be scrolled to the given index  
-            /// </summary>  
-            /// <param name="listBox">The ListBox which should be scrolled</param>  
-            /// <param name="index">The index of the item to which it should be scrolled</param>  
-            public static void OnAutoScrollToCurrentItem(ListBox listBox, int index)
-            {
+        }
 
-                if (listBox != null && listBox.Items != null && listBox.Items.Count > index && index >= 0)
+        /// <summary>  
+        /// This method will be called when the ListBox should  
+        /// be scrolled to the given index  
+        /// </summary>  
+        /// <param name="listBox">The ListBox which should be scrolled</param>  
+        /// <param name="index">The index of the item to which it should be scrolled</param>  
+        public static void OnAutoScrollToCurrentItem(ListBox listBox, int index)
+        {
 
-                    listBox.ScrollIntoView(listBox.Items[index]);
+            if (listBox != null && listBox.Items != null && listBox.Items.Count > index && index >= 0)
 
-            }
-
-            #endregion
+                listBox.ScrollIntoView(listBox.Items[index]);
 
         }
 
-
+        #endregion
 
     }
 
