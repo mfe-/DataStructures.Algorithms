120bba910ad3a9780acc8090a11aeff13040b331
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2011-10-21 15:21:40 +0000

    Elemente werden zufällig zum Canvas hinzugefügt
    git-svn-id: http://10.51.51.252/svn/Get@80 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

--- GTS/Model/Get.Model.Graph/Vertex.cs
+++ GTS/Model/Get.Model.Graph/Vertex.cs
@@ -4,7 +4,7 @@ namespace Get.Model.Graph
 {
     public class Vertex
     {
-        protected ObservableCollection<Edge> Edges = new ObservableCollection<Edge>();
+        protected ObservableCollection<Edge> _Edges = new ObservableCollection<Edge>();
 
         protected int weighted;
 
@@ -16,14 +16,21 @@ namespace Get.Model.Graph
         }
         public void addEge(Vertex pu)
         {
-            Edges.Add(new Edge(this, pu));
+            _Edges.Add(new Edge(this, pu));
         }
         public void addEge(Vertex pu, int pweighted)
         {
-            Edges.Add(new Edge(this, pu, pweighted));
+            _Edges.Add(new Edge(this, pu, pweighted));
         }
 
         public int Weighted { get { return weighted;} set {weighted = value;} }
+
+        public ObservableCollection<Edge> Edges { get { return _Edges; } set { _Edges = value; } }
+
+        public override string ToString()
+        {
+            return base.ToString() + Weighted;
+        }
 
     }
 }
---
+++ GTS/UI/Get.UI.GraphVisualization/MoveAbelItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls.Primitives;
+using System.Windows.Controls;
+
+namespace Get.UI
+{
+    public class MoveAbelItem : Thumb
+    {
+        public MoveAbelItem()
+        {
+            //Occurs one or more times as the mouse changes position when a Thumb control has logical focus and mouse capture. 
+            DragDelta += new DragDeltaEventHandler(this.MoveThumb_DragDelta);
+        }
+
+        private void MoveThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Control item = this.DataContext as Control;
+
+            if (item != null)
+            {
+                double left = Canvas.GetLeft(item);
+                double top = Canvas.GetTop(item);
+
+                Canvas.SetLeft(item, left + e.HorizontalChange);
+                Canvas.SetTop(item, top + e.VerticalChange);
+            }
+        }
+    }
+}
