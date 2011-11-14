64478d230b6682a98817b6aae001ffb12be39a6d
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2011-11-14 20:01:24 +0000

    XML XMLSerialization added to model an INotifyPropertyChanged
    git-svn-id: http://10.51.51.252/svn/Get@93 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

--- GTS/Model/Get.Model.Graph/Vertex.cs
+++ GTS/Model/Get.Model.Graph/Vertex.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.ObjectModel;
 using System.Xml.Serialization;
+using System.ComponentModel;
 
 namespace Get.Model.Graph
 {
     [XmlRoot("Vertex")]
-    public class Vertex
+    public class Vertex : INotifyPropertyChanged
     {
         protected ObservableCollection<Edge> _Edges = new ObservableCollection<Edge>();
 
@@ -25,16 +26,33 @@ namespace Get.Model.Graph
             _Edges.Add(new Edge(this, pu, pweighted));
         }
         [XmlElement("Weighted")]
-        public int Weighted { get { return weighted; } set { weighted = value; } }
+        public int Weighted { get { return weighted; } set { weighted = value; NotifyPropertyChanged("Weighted"); } }
         
         [XmlArray("Edges")]
         [XmlArrayItem("Edge", typeof(Edge))]
-        public ObservableCollection<Edge> Edges { get { return _Edges; } set { _Edges = value; } }
+        public ObservableCollection<Edge> Edges { get { return _Edges; } set { _Edges = value; NotifyPropertyChanged("Edges"); } }
 
         public override string ToString()
         {
             return base.ToString() + Weighted;
         }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notify using String property name
+        /// </summary>
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
 
     }
 }
