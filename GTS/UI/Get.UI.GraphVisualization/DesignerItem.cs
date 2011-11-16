c62d1ce4475fa47f9d5af389edc4328386c6cdbc
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2011-11-16 23:26:10 +0000

    ContentControl in DesignerItem umgewandelt um zusatz Informationen wie IsSelected zu speichern. Klassen von http://www.codeproject.com/KB/WPF/WPFDiagramDesigner_Part2.aspx
    TODO:Deslectedn wenn man in Canvas klickt
    
    
    git-svn-id: http://10.51.51.252/svn/Get@96 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

---
+++ GTS/UI/Get.UI.GraphVisualization/DesignerItem.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Get.UI
+{
+    /// <summary>
+    /// Use the DesignerItem to make your controls moveabel on a Canvas Control
+    /// http://www.codeproject.com/KB/WPF/WPFDiagramDesigner_Part2.aspx
+    /// </summary>
+    public class DesignerItem : ContentControl
+    {
+        static DesignerItem()
+        {
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(DesignerItem), new FrameworkPropertyMetadata(typeof(DesignerItem)));
+        }
+        /// <summary>
+        /// Sets IsSelected 
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseDown(e);
+            DesignerCanvas designer = VisualTreeHelper.GetParent(this) as DesignerCanvas;
+
+            if (designer != null)
+            {
+                if ((Keyboard.Modifiers & (ModifierKeys.Shift | ModifierKeys.Control)) != ModifierKeys.None)
+                {
+                    this.IsSelected = !this.IsSelected;
+                }
+                else
+                {
+                    if (!this.IsSelected)
+                    {
+                        designer.DeselectAll();
+                        this.IsSelected = true;
+                    }
+                }
+            }
+
+            e.Handled = false;
+        }
+
+        /// <summary>
+        /// Determined if the Control is selected
+        /// </summary>
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsSelectedProperty =
+          DependencyProperty.Register("IsSelected", typeof(bool),
+                                      typeof(DesignerItem),
+                                      new FrameworkPropertyMetadata(false));
+    }
+}
---
+++ GTS/UI/Get.UI.GraphVisualization/ResizeDecorator.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Get.UI
+{
+    /// <summary>
+    /// ResizeDecorator is responsible to display and hide the Adorner control
+    /// http://www.codeproject.com/KB/WPF/WPFDiagramDesigner_Part2.aspx
+    /// </summary>
+    public class ResizeDecorator : Control
+    {
+        private Adorner adorner;
+
+        public bool ShowDecorator
+        {
+            get { return (bool)GetValue(ShowDecoratorProperty); }
+            set { SetValue(ShowDecoratorProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowDecoratorProperty =
+            DependencyProperty.Register("ShowDecorator", typeof(bool), typeof(ResizeDecorator),
+            new FrameworkPropertyMetadata(false, new PropertyChangedCallback(ShowDecoratorProperty_Changed)));
+
+        public ResizeDecorator()
+        {
+            Unloaded += new RoutedEventHandler(this.ResizeDecorator_Unloaded);
+        }
+
+        private void HideAdorner()
+        {
+            if (this.adorner != null)
+            {
+                this.adorner.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void ShowAdorner()
+        {
+            if (this.adorner == null)
+            {
+                AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this);
+
+                if (adornerLayer != null)
+                {
+                    ContentControl designerItem = this.DataContext as ContentControl;
+                    Canvas canvas = VisualTreeHelper.GetParent(designerItem) as Canvas;
+                    this.adorner = new ResizeAdorner(designerItem);
+                    adornerLayer.Add(this.adorner);
+
+                    if (this.ShowDecorator)
+                    {
+                        this.adorner.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        this.adorner.Visibility = Visibility.Hidden;
+                    }
+                }
+            }
+            else
+            {
+                this.adorner.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void ResizeDecorator_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (this.adorner != null)
+            {
+                AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this);
+                if (adornerLayer != null)
+                {
+                    adornerLayer.Remove(this.adorner);
+                }
+
+                this.adorner = null;
+            }
+        }
+
+        private static void ShowDecoratorProperty_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ResizeDecorator decorator = (ResizeDecorator)d;
+            bool showDecorator = (bool)e.NewValue;
+
+            if (showDecorator)
+            {
+                decorator.ShowAdorner();
+            }
+            else
+            {
+                decorator.HideAdorner();
+            }
+        }
+    }
+    public class ResizeAdorner : Adorner
+    {
+        private VisualCollection visuals;
+        private ResizeChrome chrome;
+
+        protected override int VisualChildrenCount
+        {
+            get
+            {
+                return this.visuals.Count;
+            }
+        }
+
+        public ResizeAdorner(ContentControl designerItem)
+            : base(designerItem)
+        {
+            this.chrome = new ResizeChrome();
+            this.visuals = new VisualCollection(this);
+            this.visuals.Add(this.chrome);
+            this.chrome.DataContext = designerItem;
+        }
+
+        protected override Size ArrangeOverride(Size arrangeBounds)
+        {
+            this.chrome.Arrange(new Rect(arrangeBounds));
+            return arrangeBounds;
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return this.visuals[index];
+        }
+    }
+    public class ResizeChrome : Control
+    {
+        static ResizeChrome()
+        {
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(ResizeChrome), new FrameworkPropertyMetadata(typeof(ResizeChrome)));
+        }
+    }
+}
