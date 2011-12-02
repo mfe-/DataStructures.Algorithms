9cc7b5468667ba0b76f11863197dbe8cf801398f
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2011-12-02 18:03:29 +0000

    PositionU / PositionV will be set by binding
    git-svn-id: http://10.51.51.252/svn/Get@102 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

---
+++ GTS/UI/Get.UI.GraphVisualization/Converter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows;
+
+[assembly: XmlnsDefinition("http://schemas.get.com/winfx/2009/xaml/Graph", "Get.UI")]
+namespace Get.UI
+{
+    public static class Converters
+    {
+        public static PointAdderConverter PointAdderConverter = new PointAdderConverter();
+    }
+    public sealed class PointAdderConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null) return string.Empty;
+            if (!value.GetType().Equals((targetType))) return value;
+            if (parameter == null) return value;
+            Point pointValue = (Point)value;
+            Point pointParameter = (Point)parameter;
+            pointValue.X = pointValue.X + pointParameter.X;
+            pointValue.Y = pointValue.Y + pointParameter.Y;
+
+            return pointValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+
+        #endregion
+    }
+}
