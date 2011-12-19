760fa6a9460ad46386b3a97aa692fcc3f6a9f5af
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2011-12-19 17:40:29 +0000

    Projekte werden angezeigt
    git-svn-id: http://10.51.51.252/svn/Get@113 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

--- GTS/UI/Get.TimeKeeping/ViewModel/ViewModel.Get.cs
+++ GTS/UI/Get.TimeKeeping/ViewModel/ViewModel.Get.cs
@@ -10,7 +10,23 @@ namespace Get.UI.TimeKeeping
 {
     public class ViewModel_Get : ViewModelBase
     {
+        public ViewModel_Get()
+        {
+            using (getEntities context = new getEntities())
+            {
+                context.ContextOptions.LazyLoadingEnabled = false;
+                _Projects = context.g_project.ToObservableCollection();
+                _ProjectAssistens = context.g_projectassistent.ToObservableCollection();
 
+                // Load the orders for the customer explicitly.
+                //if (!contact.SalesOrderHeaders.IsLoaded)
+                //{
+                //    contact.SalesOrderHeaders.Load();
+                //}
+            }
+
+
+        }
         private ObservableCollection<g_project> _Projects;
         public ObservableCollection<g_project> Projects
         {
@@ -22,6 +38,41 @@ namespace Get.UI.TimeKeeping
             {
                 _Projects = value;
                 NotifyPropertyChanged(this.GetMemberName(x => x.Projects));
+            }
+        }
+
+
+        private g_project _SelectedProject;
+        public g_project SelectedProject
+        {
+            get
+            {
+                return _SelectedProject;
+            }
+            set
+            {
+                _SelectedProject = value;
+                NotifyPropertyChanged(this.GetMemberName(x => x.SelectedProject));
+                NotifyPropertyChanged(this.GetMemberName(x => x.ProjectAssistens));
+            }
+        }
+
+        private ObservableCollection<g_projectassistent> _ProjectAssistens;
+        public ObservableCollection<g_projectassistent> ProjectAssistens
+        {
+            get
+            {
+                if (SelectedProject == null) return new ObservableCollection<g_projectassistent>();
+                else
+                {
+                    var g =  _ProjectAssistens.Where(a => a.g_project.Equals(SelectedProject)).ToObservableCollection();
+                    return g;
+                }
+            }
+            set
+            {
+                _ProjectAssistens = value;
+                NotifyPropertyChanged(this.GetMemberName(x => x.ProjectAssistens));
             }
         }
     }
