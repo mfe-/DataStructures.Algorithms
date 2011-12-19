3d9377272dc7c5771571a91d9fc2bbf61806948b
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2011-12-19 16:57:10 +0000

    Projekt erstellt, für Arbeitserfassung
    git-svn-id: http://10.51.51.252/svn/Get@112 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

---
+++ GTS/UI/Get.TimeKeeping/ViewModel/ViewModel.Get.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Get.Common.Cinch;
+using Get.Common;
+using System.Collections.ObjectModel;
+
+namespace Get.UI.TimeKeeping
+{
+    public class ViewModel_Get : ViewModelBase
+    {
+
+        private ObservableCollection<g_project> _Projects;
+        public ObservableCollection<g_project> Projects
+        {
+            get
+            {
+                return _Projects;
+            }
+            set
+            {
+                _Projects = value;
+                NotifyPropertyChanged(this.GetMemberName(x => x.Projects));
+            }
+        }
+    }
+}
