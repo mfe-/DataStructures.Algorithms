0b1fc57b0d3a1b8c94e69d2e831f31b59608bcfb
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2014-01-31 19:18:40 +0000

    git-svn-id: http://10.51.51.252/svn/Get@194 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

---
+++ GTS/Common/Get.Algorithms.DataStrucre.Test/Sort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Get.Algorithms.DataStrucre.Test
+{
+    [TestClass]
+    public class Sort
+    {
+        [TestMethod]
+        public void Quick_Sort()
+        {
+            var a = new int[] { 4, 1, 2, 3, 7, 8, 6, 5 }.Quick_Sort();
+            var b = new int[] { 10, 11, 1, 0, 101, 111, 01, 00 }.Quick_Sort();
+
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }.ToList(), a.ToList());
+            CollectionAssert.AreEqual(new int[] { 0, 0, 1, 1, 10,11, 101, 111 }.ToList(), b.ToList());
+
+        }
+    }
+}
