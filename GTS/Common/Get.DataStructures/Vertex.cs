6bca720c68decdacf19c2be5b55c21bf809ddb14
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2014-02-01 13:57:56 +0000

    git-svn-id: http://10.51.51.252/svn/Get@198 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

--- GTS/Common/Get.DataStructures/Vertex.cs
+++ GTS/Common/Get.DataStructures/Vertex.cs
@@ -2,47 +2,103 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace Get.DataStructure
 {
-    public class Vertex<W, T> : IVertex<W, T>
+    [DebuggerDisplay("Edge = {Weighted},U={U}, V = {V}")]
+    public class Vertex<W> : IVertex<W>
         where W : IComparable<W>
     {
-        public W Weight
+
+        #region Members
+        protected IEnumerable<IEdge<W>> _Edges;
+        protected Guid _Guid;
+        protected W weight;
+        #endregion
+
+        public Vertex()
+        {
+            this._Guid = Guid.NewGuid();
+            this._Edges = new List<IEdge<W>>();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Vertex class that contains the specified weighted.
+        /// </summary>
+        /// <param name="pweighted"></param>
+        public Vertex(W weight)
+            : this()
+        {
+            this.weight = weight;
+        }
+        /// <summary>
+        /// Gets or sets the Weighted of the vertex
+        /// </summary>
+        public virtual W Weight { get { return this.weight; } set { this.weight = value; } }
+
+        /// <summary>
+        /// Gets or sets the list of edges which connects the vertex neighbours
+        /// </summary>
+        public IEnumerable<IEdge<W>> Edges { get { return _Edges; } set { _Edges = value; } }
+
+        /// <summary>
+        /// Amount of neighbours
+        /// </summary>
+        public int Size
         {
             get
             {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
+                return Edges.Count(); //Knotengrad
             }
         }
 
-        public T Data
+
+
+        public virtual IEdge<W> AddEdge(IVertex<W> U, W Weight, bool Undirected)
         {
-            get
+            IEdge<W> e1 = new Edge<W>(this, U, Weight);
+            _Edges.ToList<IEdge<W>>().Add(e1);
+            if (Undirected == true)
             {
-                throw new NotImplementedException();
+                U.AddEdge(this, Weight, false);
             }
-            set
-            {
-                throw new NotImplementedException();
-            }
+
+            return e1;
         }
 
-
-        public IEnumerable<IEdge<W, T>> Edges
+        public virtual void RemoveEdge(IVertex<W> U, bool Undirected)
         {
-            get
+            IEdge<W> edge = this.Edges.Where(a => a.U.Equals(this) && a.V.Equals(U)).FirstOrDefault<IEdge<W>>();
+
+            if (Undirected.Equals(false))
             {
-                throw new NotImplementedException();
+                IEdge<W> edged = edge.V.Edges.Where(a => a.U.Equals(edge.V) && a.V.Equals(this) && a.Weight.Equals(edge.Weight)).FirstOrDefault<IEdge<W>>();
+
+                edge.V.Edges.ToList<IEdge<W>>().Remove(edged);
             }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            this.Edges.ToList<IEdge<W>>().Remove(edge);
+        }
+
+        /// <summary>
+        /// Determines with the guid whether the specified Object is equal to the current Object.
+        /// http://msdn.microsoft.com/en-us/library/bsc2ak47.aspx
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object. </param>
+        /// <returns>true if the specified Object is equal to the current Object; otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            if (!obj.GetType().Equals(typeof(IVertex<W>))) return false;
+
+            return this._Guid.Equals((obj as Vertex<W>)._Guid);
+        }
+        /// <summary>
+        /// Serves as a hash function for a particular type. 
+        /// </summary>
+        /// <returns>A hash code for the current Object.</returns>
+        public override int GetHashCode()
+        {
+            return _Guid.GetHashCode();
         }
     }
 }
