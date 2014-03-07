65eaf7623d75b277773a490c82dd2fe06d7d142c
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2014-03-07 15:40:46 +0000

    Test
    git-svn-id: http://10.51.51.252/svn/Get@204 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

---
+++ GTS/Common/Get.Algorithms.Graph.Test/Extensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Get.DataStructure;
+using Get.Algorithms.Graph;
+using System.Collections.ObjectModel;
+
+namespace Get.Algorithms.Graph.Test
+{
+    [TestClass]
+    public class Extensions
+    {
+        public class Rail{ }
+        public class RailEdge : Edge<double, Rail>
+        {
+            public RailEdge()
+            {
+                this.U = new ConnectionVertex();
+            }
+        }
+        public class ConnectionVertex : Vertex<double, Rail>
+        {
+            public ConnectionVertex()
+            {
+                this._Edges = new ObservableCollection<RailEdge>();
+
+            }
+        }
+        [TestMethod]
+        public void ExtensionCalls()
+        {
+            Vertex<int, object> v1 = new Vertex<int, object>();
+            v1.Depth_First_Traversal<int, object>(new List<Vertex<int, object>>());
+
+            ConnectionVertex connv = new ConnectionVertex();
+            connv.Depth_First_Traversal(new List<ConnectionVertex>());
+
+
+        }
+    }
+}
