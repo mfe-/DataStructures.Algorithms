3f91280899c8850d7f16cabc0565f4dbc41acc5d
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2015-06-21 18:21:17 +0000

    Get.the.Solution.DataStructure Project / Folder rename
    git-svn-id: http://10.51.51.252/svn/Get@217 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

--- GTS/Common/Get.DataStructure.Test/DataStructure.cs
+++ GTS/Common/Get.DataStructure.Test/DataStructure.cs
@@ -3,7 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Get.DataStructure;
+using Get.the.Solution.DataStructure;
 
 namespace Get.the.Solution.DataStructure.Test
 {
