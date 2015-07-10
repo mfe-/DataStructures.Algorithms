4f3ddf85c7f93b1cda5bc6dbe858b04def455cc9
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2015-07-10 07:29:03 +0000

    First Draft of INodes
    git-svn-id: http://10.51.51.252/svn/Get@220 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

---
+++ GTS/Common/Get.the.Solution.DataStructure.Test/Node.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Get.the.Solution.DataStructure.Test.ExtendedModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Get.the.Solution.DataStructure.Test
+{
+    [TestClass]
+    public class Node
+    {
+        [TestMethod]
+        public void TestNodeSetLeft()
+        {
+            Node<int> node1 = new Node<int>();
+            Node<int> node2 = new Node<int>();
+
+            node1.SetLeft(node2);
+
+            var testResult = node1.GetLeft();
+
+            Assert.AreEqual(node2, testResult);
+
+            //extended node class test
+            SingleNodeExtended<int> node3extended = new SingleNodeExtended<int>();
+
+            node2.SetRight(new SingleNode<int>());
+
+        }
+        [TestMethod]
+        public void TestNodeSetRight()
+        {
+            Node<int> node1 = new Node<int>();
+            Node<int> node2 = new Node<int>();
+
+            node1.SetRight(node2);
+
+            var testResult = node1.GetRight();
+
+            Assert.AreEqual(node2, testResult);
+
+        }
+    }
+}
---
+++ GTS/Common/Get.the.Solution.DataStructures/Interfaces/IInvariant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Get.the.Solution.DataStructure
+{
+    /// <summary>
+    /// Invarainz: Typen bleiben gleich
+    /// </summary>
+    /// <remarks>https://msdn.microsoft.com/en-us/library/dd997386.aspx</remarks>
+    /// <typeparam name="T">The type which should be invariant</typeparam>
+    public interface IInvariant<T> : ICovariant<T>, IContravariant<T> { }
+}
