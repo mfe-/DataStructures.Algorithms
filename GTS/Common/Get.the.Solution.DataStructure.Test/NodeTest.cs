5b3eda6c17dc0b50582d58c94265fd13c4a6c66d
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2015-07-17 16:21:31 +0000

    ISingleNode and INode complete - Tests successful
    git-svn-id: http://10.51.51.252/svn/Get@226 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

--- GTS/Common/Get.the.Solution.DataStructure.Test/NodeTest.cs
+++ GTS/Common/Get.the.Solution.DataStructure.Test/NodeTest.cs
@@ -14,7 +14,56 @@ namespace Get.the.Solution.DataStructure.Test
         [TestMethod]
         public void TestNodeSetLeft()
         {
-          
+            Node<int> node1 = new Node<int>() { Value = 1 };
+            Node<int> node2 = new Node<int>() { Value = 2 };
+            //test get Left node for extended class 1->2
+            node1.Left = node2;
+            var testResult6 = node1.Left;
+            Assert.AreEqual(node2, testResult6);
+
+            //assign node extended 1->2->3
+            NodeExtended<int> nodeExtended = new NodeExtended<int>() { Value = 3 };
+            node2.Left = nodeExtended;
+            var testResult7 = node2.Left;
+            Assert.AreEqual(nodeExtended, testResult7);
+
+            //assign node 1->2->3->4
+            Node<int> node3 = new Node<int>(4);
+            nodeExtended.Left = node3;
+            var testResult8 = nodeExtended.Left;
+            Assert.AreEqual(node3, testResult8);
+
+            //check references
+            INode<int> startNode = node1;
+            List<Type> typeList = new List<Type>();
+            List<int> valueResult = new List<int>();
+            typeList = new List<Type>();
+            valueResult = new List<int>();
+
+            while (startNode != null)
+            {
+                typeList.Add(startNode.GetType());
+                valueResult.Add(startNode.Value);
+                startNode = startNode.Left;
+            }
+
+            var expectedTypeValues = new List<Type>()
+            {
+                typeof(Node<int>),
+                typeof(Node<int>),
+                typeof(NodeExtended<int>),
+                typeof(Node<int>)
+            };
+            var expectedValueValues = new List<int>()
+            {
+                1,
+                2,
+                3,
+                4
+            };
+
+            CollectionAssert.AreEqual(typeList, expectedTypeValues);
+            CollectionAssert.AreEqual(valueResult, expectedValueValues);
 
         }
         [TestMethod]
@@ -31,52 +80,107 @@ namespace Get.the.Solution.DataStructure.Test
             //test call extension on base node - should compileable
             node1.Left.Travers();
 
-            //assign node to singlenode
+            //assign node to singlenode 3->1
             SingleNode<int> singlenode = new SingleNode<int>(3);
             singlenode.Right = node1;
             var testResult2 = singlenode.Right;
             Assert.AreEqual(node1, testResult2);
 
-            //assign singlenode to singleNodeExtended
-            SingleNodeExtended<int> node3Extended = new SingleNodeExtended<int>() { Value = 3 };
-            singlenode.Right = node3Extended;
-            var testResult3 = singlenode.Right;
+            //assign singlenode to singleNodeExtended 3->1->4
+            SingleNodeExtended<int> node3Extended = new SingleNodeExtended<int>() { Value = 4 };
+            singlenode.Right.Right = node3Extended;
+            var testResult3 = singlenode.Right.Right;
             Assert.AreEqual(node3Extended, testResult3);
 
-            SingleNode<int> singlenode1 = new SingleNode<int>(3);
+            //assign node to singlenode 3->1->4->5
+            SingleNode<int> singlenode1 = new SingleNode<int>(5);
             node3Extended.Right = singlenode1;
             var testResult4 = node3Extended.Right;
             Assert.AreEqual(singlenode1, testResult4);
 
-            //assign node to singlenode
-            singlenode1.Right = node1;
+            //assign node to singlenode 3->1->4->5->2
+            singlenode1.Right = node2;
             var testResult5 = singlenode1.Right;
-            Assert.AreEqual(node1, testResult5);
+            Assert.AreEqual(node2, testResult5);
 
-            //check for types
-            //SingleNode<int> start = singlenode;
-            //List<String> typeList = new List<string>();
+            //check references
+            ISingleNode<int> start = singlenode;
+            List<Type> typeList = new List<Type>();
+            List<int> valueResult = new List<int>();
 
-            //while (start != null)
-            //{
-            //    typeList.Add(start.GetType().ToString());
-            //    start.Right = start;
-            //}
+            while (start != null)
+            {
+                typeList.Add(start.GetType());
+                valueResult.Add(start.Value);
+                start = start.Right;
+            }
 
-            //test get Left node for extended class
+            List<Type> expectedTypeValues = new List<Type>()
+            {
+                typeof(SingleNode<int>),
+                typeof(Node<int>),
+                typeof(SingleNodeExtended<int>),
+                typeof(SingleNode<int>),
+                typeof(Node<int>)
+            };
+            List<int> expectedValueValues = new List<int>()
+            {
+                3,
+                1,
+                4,
+                5,
+                2
+            };
+
+            CollectionAssert.AreEqual(typeList, expectedTypeValues);
+            CollectionAssert.AreEqual(valueResult, expectedValueValues);
+
+            //test get Left node for extended class 1->2
             node1.Right = node2;
             var testResult6 = node1.Right;
             Assert.AreEqual(node2, testResult6);
 
+            //assign node extended 1->2->3
             NodeExtended<int> nodeExtended = new NodeExtended<int>() { Value = 3 };
             node2.Right = nodeExtended;
             var testResult7 = node2.Right;
             Assert.AreEqual(nodeExtended, testResult7);
 
-            Node<int> node3 = new Node<int>();
+            //assign node 1->2->3->4
+            Node<int> node3 = new Node<int>(4);
             nodeExtended.Right = node3;
             var testResult8 = nodeExtended.Right;
             Assert.AreEqual(node3, testResult8);
+
+            //check references
+            INode<int> startNode = node1;
+            typeList = new List<Type>();
+            valueResult = new List<int>();
+
+            while (startNode != null)
+            {
+                typeList.Add(startNode.GetType());
+                valueResult.Add(startNode.Value);
+                startNode = startNode.Right;
+            }
+
+            expectedTypeValues = new List<Type>()
+            {
+                typeof(Node<int>),
+                typeof(Node<int>),
+                typeof(NodeExtended<int>),
+                typeof(Node<int>)
+            };
+            expectedValueValues = new List<int>()
+            {
+                1,
+                2,
+                3,
+                4
+            };
+
+            CollectionAssert.AreEqual(typeList, expectedTypeValues);
+            CollectionAssert.AreEqual(valueResult, expectedValueValues);
 
         }
 
