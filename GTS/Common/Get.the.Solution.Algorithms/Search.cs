c554236422a8435675c1b8ac11fc59c3a488bfa0
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2015-08-15 07:53:14 +0000

    Copy/Paste from Repository Algodat\Get.Algorithms Search \Sort
    git-svn-id: http://10.51.51.252/svn/Get@229 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

--- GTS/Common/Get.the.Solution.Algorithms/Search.cs
+++ GTS/Common/Get.the.Solution.Algorithms/Search.cs
@@ -9,20 +9,175 @@ namespace Get.the.Solution.Algorithms
     using Get.the.Solution.DataStructure;
     public static class Search
     {
-        public static IEnumerable<INode<T>> InOrder<T>(this INode<T> p, IList<INode<T>> list)
-        {
-            if (p != null)
-            {
-                InOrder(p.Left, list);
-                list.Add(p);
-                InOrder(p.Right, list);
-            }
-            return list;
-        }
         public static IEnumerable<INode<T>> InOrder<T>(this INode<T> p)
         {
             return InOrder(p, new List<INode<T>>());
         }
+        public static IEnumerable<INode<T>> InOrder<T>(this INode<T> p, IList<INode<T>> list)
+        {
+            if (p != null)
+            {
+                InOrder<T>(p.Left, list);
+                list.Add(p);
+                InOrder<T>(p.Right, list);
+            }
+            return list;
+        }
 
+        public static IEnumerable<INode<T>> PreOrder<T>(this INode<T> p)
+        {
+            return PreOrder(p, new List<INode<T>>());
+        }
+        public static IEnumerable<INode<T>> PreOrder<T>(this INode<T> p, IList<INode<T>> list)
+        {
+            if (p != null)
+            {
+                list.Add(p);
+                PreOrder<T>(p.Left, list);
+                PreOrder<T>(p.Right, list);
+            }
+            return list;
+        }
+        public static IEnumerable<INode<T>> PostOrder<T>(this INode<T> p)
+        {
+            return PostOrder(p, new List<INode<T>>());
+        }
+
+        public static IEnumerable<INode<T>> PostOrder<T>(this INode<T> p, IList<INode<T>> list)
+        {
+            if (p != null)
+            {
+                PostOrder<T>(p.Left, list);
+                PostOrder<T>(p.Right, list);
+                list.Add(p);
+            }
+            return list;
+        }
+        public static ITreeNode<T> CreatePostOrderList<T>(this ITreeNode<T> p)
+        {
+            ITreeNode<T> temp = CreatePostOrderList<T>(p, null);
+            temp.Left = null;
+            temp = temp.Parent;
+            return temp;
+        }
+        private static ITreeNode<T> CreatePostOrderList<T>(this ITreeNode<T> p, ITreeNode<T> currentNode)
+        {
+            if (p != null)
+            {
+                currentNode = CreatePostOrderList<T>(p.Left, currentNode);
+                currentNode = CreatePostOrderList<T>(p.Right, currentNode);
+                //first item
+                if (currentNode == null)
+                {
+                    currentNode = p;
+                    //save first element
+                    currentNode.Parent = p;
+                }
+                else
+                {
+                    currentNode.Left = p;
+                    p.Parent = currentNode.Parent;
+                    p.Right = currentNode;
+                    currentNode = p;
+                }
+            }
+            return currentNode;
+        }
+
+
+        public static T BinarySearch<T>(this IEnumerable<T> A, T key) where T : IComparable<T>
+        {
+            return BinarySearch(A.ToArray(), key, 0, A.Count() - 1);
+        }
+        private static T BinarySearch<T>(this IList<T> A, T key, int l, int r) where T : IComparable<T>
+        {
+            //5.CompareTo(6) = -1      First int is smaller.
+            //6.CompareTo(5) =  1      First int is larger.
+            //5.CompareTo(5) =  0      Ints are equal.
+            int m;
+            do
+            {
+                m = (int)System.Math.Round((double)(l + r) / 2, 0);
+                //key < A[m]
+                if (key.CompareTo(A[m]) == -1)
+                {
+                    r = m - 1;
+                }
+                else
+                {
+                    l = m + 1;
+                }
+            }
+            while (key.CompareTo(A[m]) != 0 || l.CompareTo(r) == -1);
+            if (key.CompareTo(A[m]) == 0)
+            {
+                return A[m];
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+
+        }
+        /// <summary>
+        /// do not use - bad runtime
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int FibonacciSearch(this IList<int> A, int s)
+        {
+            int p = 0, q = 0, i = 1;
+            int prep = 0, preq = 0, prei = 0;
+            while ((q + p) < A.Count)
+            {
+                q = p;
+                p = i;
+                i = q + p;
+                if ((q + p) < A.Count)
+                {
+                    prep = p;
+                    preq = q;
+                    prei = i;
+                }
+            }
+
+            return FibonacciSearch(A, s, prei, prep, preq);
+        }
+
+        public static int FibonacciSearch(this IList<int> A, int s, int i, int p, int q)
+        {
+            //5.CompareTo(6) = -1      First int is smaller.
+            //6.CompareTo(5) =  1      First int is larger.
+            //5.CompareTo(5) =  0      Ints are equal.
+            System.Diagnostics.Debug.WriteLine("s={0},i={1}p={2}q={3}", s, i, p, q);
+            if (s < A[i - 1])
+            {
+                if (q == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return FibonacciSearch(A, s, i - q, q, p - q);
+                }
+
+            }
+            else if (s > A[i - 1])
+            {
+                if (p == 1)
+                {
+                    return 0;
+                }
+                else
+                    return FibonacciSearch(A, s, i + q, p - q, (2 * q) - p);
+            }
+            else
+            {
+
+                return i - 1;
+            }
+        }
     }
+
 }
--- GTS/Common/Get.the.Solution.Algorithms/Sort.cs
+++ GTS/Common/Get.the.Solution.Algorithms/Sort.cs
@@ -8,24 +8,130 @@ namespace Get.the.Solution.Algorithms
 {
     public static class Sort
     {
-        //public static IEnumerable<T> Selection_Sort<T>(this IEnumerable<T> A) where T : IComparable<T>
-        //{
-        //    for (int j = 0; j < A.Count(); j++)
-        //    {
-        //        int minpos = j;
-        //        for (int i = j + 1; j < A.Count(); i++)
-        //        {
-        //            if (A.ToArray()[i].CompareTo(A.ToArray()[minpos]) == -1) // a<b equals to a.compare(b)==-1
-        //                minpos = i;
-        //        }
-        //        if (minpos > j)
-        //        {
-        //            //vertauschen
-        //        }
-        //    }
+        /// <summary>
+        /// von algodat übung 1 ss15 beispiel 8 ähnlich wie insertion sort. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Sub_Sort<T>(this IList<T> A) where T : IComparable<T>
+        {
+            for (int j = 1; j < A.Count; j++)
+            {
+                A.Print();
+                Sub_Sort(A, j - 1, A[j]);
+            }
+            return A;
+        }
+        private static IEnumerable<T> Sub_Sort<T>(this IList<T> A, int i, T k) where T : IComparable<T>
+        {
+            //assign k to A[i + 1] when greater else use A[i]
+            A[i + 1] = k.CompareTo(A[i]) == 1 ? k : A[i];
+            //A[i]>k
+            if (A[i].CompareTo(k) == 1) //change compareTo==-1 and ? A[i] : k; for descending
+            {
+                if (i == 0)
+                {
+                    A[i] = k;
+                }
+                else
+                {
+                    A.Print();
+                    Sub_Sort(A, i - 1, k);
+                }
+            }
+            return A;
+        }
+        public static IEnumerable<T> Transpose_Sort<T>(this IList<T> A) where T : IComparable<T>
+        {
+            if (A.Count < 1)
+            {
+                return A;
+            }
+            for (int i = 0; i < A.Count - 1; i++)
+            {
+                Transpose_Sort(A, i);
+            }
+            return A;
+        }
+        private static IEnumerable<T> Transpose_Sort<T>(this IList<T> A, int i) where T : IComparable<T>
+        {
+            if (A[i].CompareTo(A[i + 1]) != -1) // a<b equals to a.compare(b)==-1
+            {
+                //transpose a[i] and a[j]
+                T tmp = A[i];
+                A[i] = A[i + 1];
+                A[i + 1] = tmp;
+                if (i > 0)
+                {
+                    Transpose_Sort(A, i - 1);
+                }
+            }
+            return A;
+        }
+        /// <summary>
+        /// Basically looksup for the proper element and moves to the left. All left elements behind the current element are sorted.  
+        /// Less data moving
+        /// A lot key comparision
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Selection_Sort<T>(this IList<T> A) where T : IComparable<T>
+        {
+            for (int j = 0; j < A.Count(); j++)
+            {
+                //determine position of minimum key
+                int minpos = j;
+                for (int i = j + 1; i < A.Count(); i++)
+                {
+                    if (A[i].CompareTo(A[minpos]) == -1) // a<b equals to a.compare(b)==-1
+                    {
+                        minpos = i;
+                    }
+                }
 
-        //    return A;
-        //}
+                if (minpos > j)
+                {
+                    //transpose a[i] and a[j]
+                    T tmp = A[minpos];
+                    A[minpos] = A[j];
+                    A[j] = tmp;
+                }
+                A.Print();
+            }
+
+            return A;
+        }
+        /// <summary>
+        /// Basically compares all keys behind the current position and move to the correct place
+        /// Less key comparision
+        /// A lot data moving
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Insertion_Sort<T>(this IList<T> A) where T : IComparable<T>
+        {
+            T key;
+            int i;
+            for (int j = 1; j < A.Count; j++)
+            {
+                key = A[j];
+                //insert A[j] in sorted sequence A[0],...,A[j-1]
+                i = j - 1;
+                while (i >= 0 && A[i].CompareTo(key) == 1)
+                {
+                    //shift elements backward and calculate position of the new key
+                    A[i + 1] = A[i];
+                    i = i - 1;
+                }
+
+                A[i + 1] = key;
+                Print(A);
+            }
+            return A;
+        }
         private static IEnumerable<T> Quick_Sort<T>(this IList<T> A, int l, int r) where T : IComparable<T>
         {
             if (l < r)
@@ -33,20 +139,20 @@ namespace Get.the.Solution.Algorithms
                 T x = A[r];
                 //partition
                 int i = l;
-                int j = r-1;
+                int j = r - 1;
                 T tmp;
                 do
                 {
                     //5.CompareTo(6) = -1      First int is smaller.
                     //6.CompareTo(5) =  1      First int is larger.
                     //5.CompareTo(5) =  0      Ints are equal.
-                    while ((A[i].CompareTo(x)==-1 || A.ToArray()[i].CompareTo(x)==0) && i < r) 
+                    while ((A[i].CompareTo(x) == -1 || A.ToArray()[i].CompareTo(x) == 0) && i < r)
                     {
                         i = i + 1;
                     }
-                    while (A[j].CompareTo(x)==1 && j > l)
+                    while (A[j].CompareTo(x) == 1 && j > l)
                     {
-                        j=j-1;
+                        j = j - 1;
                     }
 
                     if (i < j)
@@ -66,15 +172,16 @@ namespace Get.the.Solution.Algorithms
 
                 int p = i;
 
-                Quick_Sort(A, l, p-1);
-                Quick_Sort(A, p+1, r);
+                Quick_Sort(A, l, p - 1);
+                Quick_Sort(A, p + 1, r);
             }
             return A;
         }
         public static IEnumerable<T> Quick_Sort<T>(this IEnumerable<T> A) where T : IComparable<T>
         {
-            return Quick_Sort<T>(A.ToList(), 0, A.Count()-1);
+            return Quick_Sort<T>(A.ToList(), 0, A.Count() - 1);
         }
+
         public static T Min<T>(params T[] values) where T : IComparable<T>
         {
             T min = values[0];
@@ -85,75 +192,6 @@ namespace Get.the.Solution.Algorithms
             }
             return min;
         }
-
-        ////http://xbfish.com/2011/11/03/insertion-sort-in-c/
-        //public static int[] Insertion_Sort(int[] A)
-        //{
-        //    for (int j = 1; j < A.Length; j++)
-        //    {
-        //        int key = A[j];
-        //        int i = j - 1;
-        //        while (i >= 0 && A[i] > key)
-        //        {
-
-        //            A[i + 1] = A[i];
-        //            i--;
-        //        }
-        //        A[i + 1] = key;
-
-        //    }
-
-        //    return A;
-        //}
-        //public static void MergeSort(int l, int r)
-        //{
-        //    if (l < r)
-        //    {
-        //        int m = (l + r) / 2;
-        //        MergeSort(l, m);
-        //        MergeSort(m + 1, r);
-
-        //        Merge(l, m, r);
-        //    }
-        //}
-
-        //public static void Merge(int l, int m, int r)
-        //{
-        //    int[] temp = new int[data.Length];
-
-        //    int i = l, h = l, j = m + 1;
-
-        //    while (h <= m && j <= r)
-        //    {
-        //        if (data[h] <= data[j])
-        //        {
-        //            temp[i] = data[h];
-        //            h++;
-        //        }
-        //        else
-        //        {
-        //            temp[i] = data[j];
-        //            j++;
-        //        }
-
-        //        i++;
-        //    }
-
-        //    if (h > m)
-        //        for (int k = j; k <= r; k++)
-        //            temp[i++] = data[k];
-        //    else
-        //        for (int k = h; k <= m; k++)
-        //            temp[i++] = data[k];
-
-        //    for (int k = l; k <= r; k++)
-        //    {
-        //        data[k] = temp[k];
-        //        System.Diagnostics.Debug.Write(" " + data[k]);
-
-        //    } System.Diagnostics.Debug.WriteLine(" m " + m + "\n");
-        //}
-
         public static void Print<T>(this IEnumerable<T> A)
         {
             if (Debugger.IsAttached)
@@ -168,11 +206,11 @@ namespace Get.the.Solution.Algorithms
             {
                 if (Debugger.IsAttached)
                 {
-                    Debug.Write(v+", ");
+                    Debug.Write(v + ", ");
                 }
                 else
                 {
-                    System.Console.Write(v+", ");
+                    System.Console.Write(v + ", ");
                 }
             }
 
