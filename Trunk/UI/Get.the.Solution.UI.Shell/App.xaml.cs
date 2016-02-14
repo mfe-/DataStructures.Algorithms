8ab0db3c0ef691cf1fbdd8c620f19cb539ab7b00
martin <martin@37f1dd3d-e751-4e8c-a1a5-e4797081b48e>
2016-02-14 10:52:57 +0000

    fixed compiling error
    git-svn-id: http://10.51.51.252/svn/Get@259 37f1dd3d-e751-4e8c-a1a5-e4797081b48e

--- Trunk/UI/Get.the.Solution.UI.Shell/App.xaml.cs
+++ Trunk/UI/Get.the.Solution.UI.Shell/App.xaml.cs
@@ -44,10 +44,10 @@ namespace Get.the.Solution.UI.Shell
         {
 
 #if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                this.DebugSettings.EnableFrameRateCounter = true;
-            }
+            //if (System.Diagnostics.Debugger.IsAttached)
+            //{
+            //    this.DebugSettings.EnableFrameRateCounter = true;
+            //}
 #endif
 
             Frame rootFrame = Window.Current.Content as Frame;
