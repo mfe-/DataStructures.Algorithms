b9c9e4ba34efa656ad1b60259ffbffebae09ad31
Martin Fetzel <martin.fetzel@get-the-solution.net>
2019-09-18 19:52:12 +0200

    Fixed type load on deserialization

--- UI/StateMachineEngine/StateResolver.cs
+++ UI/StateMachineEngine/StateResolver.cs
@@ -13,15 +13,11 @@ namespace StateMachineEngine
             {
                 return typeof(Vertex<object>);
             }
-            //if (typeName.Contains("VertexOfStateModule"))
-            //{
-            //    return typeof(Vertex<IState>);
-            //}
-            //else
-            //{
-            // Defer to the known type resolver
+            if (typeName.Contains(typeof(Vertex<IState>).ToString()))
+            {
+                return typeof(Vertex<IState>);
+            }
             return knownTypeResolver.ResolveName(typeName, typeNamespace, declaredType, null);
-            //}
         }
 
         public override bool TryResolveType(Type type, Type declaredType, DataContractResolver knownTypeResolver, out XmlDictionaryString typeName, out XmlDictionaryString typeNamespace)
@@ -29,16 +25,10 @@ namespace StateMachineEngine
             //forward the interface type of an actual implementation
             if (typeof(Vertex<IState>) == type)
             {
-                //Graph.CreateVertexFunc creates Vertices of Type Vertex<IState> but deserializing them will create Vertex<StateModule>...
-                //because of this currently editing of loaded vertices is not possible as the cast is done wrong
-
-                Type StateType = typeof(Vertex<StateModule>);
-                return knownTypeResolver.TryResolveType(StateType, declaredType, null, out typeName, out typeNamespace);
-
-                //XmlDictionary dictionary = new XmlDictionary();
-                //typeName = dictionary.Add("VertexOfModuleFunction");
-                //typeNamespace = dictionary.Add("http://schemas.get.com/winfx/2009/xaml/ModuleFunction");
-                //return true; // indicating that this resolver knows how to handle "Dog"
+                XmlDictionary dictionary = new XmlDictionary();
+                typeName = dictionary.Add(typeof(Vertex<IState>).ToString());
+                typeNamespace = dictionary.Add("http://schemas.get.com/winfx/2009/xaml/IState");
+                return true;
             }
             else
             {
