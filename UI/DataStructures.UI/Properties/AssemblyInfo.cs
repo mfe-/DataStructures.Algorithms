c223462eb065a690094343a21bfd1591c89a16d3
Martin Fetzel <martin.fetzel@get-the-solution.net>
2019-10-01 19:37:33 +0200

    port wpf .NET control to netcoreapp3

--- UI/DataStructures.UI/Properties/AssemblyInfo.cs
+++ UI/DataStructures.UI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Abstract.DataStructures.UI")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright ©  2011-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
