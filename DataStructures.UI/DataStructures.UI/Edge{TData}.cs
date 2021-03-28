320f071117a5d8213b269e967a20374a0c9815df
Martin Fetzel <martin.fetzel@get-the-solution.net>
2021-03-28 17:55:54 +0200

    Add Edge<TData>

---
+++ DataStructures.UI/DataStructures.UI/Edge{TData}.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace DataStructures.UI
+{
+    [DebuggerDisplay("U={U}->V={V},Edge ={Weighted},Value={Value}")]
+    [DataContract(Namespace = "http://schemas.get.com/Graph/Edges")]
+    public class Edge<TData> : Edge, IEdge<TData>
+    {
+        /// <summary>
+        /// Initializes a new instance of the Edge class.
+        /// </summary>
+        /// <param name="u">Vertex of the Edge</param>
+        /// <param name="v">Vertex of the Edge</param>
+        public Edge(IVertex u, IVertex v) : base(u, v)
+        {
+        }
+        /// <summary>
+        /// Initializes a new instance of the Edge class.
+        /// </summary>
+        /// <param name="u">Vertex of the Edge</param>
+        /// <param name="v">Vertex of the Edge</param>
+        /// <param name="weighted">Sets the Weighted of the Edge</param>
+        public Edge(IVertex u, IVertex v, double weighted) : base(u, v, weighted)
+        {
+        }
+
+        /// <summary>
+        /// Get or sets the value of the edge
+        /// </summary>
+        [DataMember(Name = "Value", IsRequired = false)]
+        public TData Value { get; set; }
+
+    }
+}
--- DataStructures.UI/DataStructures.UI/Vertex{TData}.cs
+++ DataStructures.UI/DataStructures.UI/Vertex{TData}.cs
@@ -5,7 +5,7 @@ namespace DataStructures.UI
 {
     [DebuggerDisplay("Vertex={Weighted},Value={Value},GUID={_Guid}")]
     [DataContract(Namespace = "http://schemas.get.com/Graph/Vertex")]
-    public class Vertex<TData> : DataStructures.UI.Vertex, IVertex<TData>
+    public class Vertex<TData> : Vertex, IVertex<TData>
     {
         /// <summary>
         /// Initializes a new instance of the Vertex class.
@@ -27,7 +27,7 @@ namespace DataStructures.UI
 
         public override IEdge CreateEdge(IVertex u, double weighted = 0)
         {
-            IEdge e1 = new DataStructures.UI.Edge(this, u, weighted);
+            IEdge e1 = new Edge<TData>(this, u, weighted);
             return e1;
         }
     }
